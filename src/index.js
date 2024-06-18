3a17adc3b69b0fe3ad065e5a8a285d8c86b8aca6
slpkid <153894144+slpkid@users.noreply.github.com>
2024-06-17 17:46:46 -0700

    added infrastructure for the about and menu pages.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import { renderHome, contentDiv } from "./home";
 import { clearNodes } from "./clearNode";
+import { renderMenu } from './menu'
+import { renderAbout } from "./about";
 
 const homeButton = document.getElementById('home');
 const menuButton = document.getElementById('menu');
 const aboutButton = document.getElementById('about');
+
+const clearContent = () => {
+    clearNodes(contentDiv)
+}
 
 (function init() {
     renderHome()
 })();
 
 
-homeButton.addEventListener('click', () => {
-    clearNodes(contentDiv);
+
+homeButton.addEventListener('click', (e) => {
+    clearContent();
     renderHome();
+})
+
+menuButton.addEventListener('click', (e) => {
+    clearContent();
+    renderMenu();
+})
+
+aboutButton.addEventListener('click', (e) => {
+    clearContent();
+    renderAbout();
 })
